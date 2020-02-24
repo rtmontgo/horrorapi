24d6fba45a79a265aeddd245da67924fcb71045e
Taylor <r.taylor.montgomery@gmail.com>
2020-02-23 23:05:34 -0600

    3.3 initial components and views

--- client/dist/src.78399e21.js
+++ client/dist/src.78399e21.js
@@ -31887,7 +31887,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50010" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50563" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
---
+++ client/src/components/movie-view/movie-view.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+export class MovieView extends React.Component {
+
+  constructor() {
+    super();
+
+    this.state = {};
+  }
+
+  render() {
+    const { movie } = this.props;
+
+    if (!movie) return null;
+
+    return (
+      <div className="movie-view">
+        <img className="movie-poster" src={movie.ImagePath} />
+        <div className="movie-title">
+          <span className="label">Title: </span>
+          <span className="value">{movie.Title}</span>
+        </div>
+        <div className="movie-description">
+          <span className="label">Description: </span>
+          <span className="value">{movie.Description}</span>
+        </div>
+
+        <div className="movie-genre">
+          <span className="label">Genre: </span>
+          <span className="value">{movie.Genre.Name}</span>
+        </div>
+        <div className="movie-director">
+          <span className="label">Director: </span>
+          <span className="value">{movie.Director.Name}</span>
+        </div>
+        <Button block variant="outline-primary" onClick={event => handleSubmit(event)}>Add to Favorites</Button>
+        <Link to={`/`}><Button block variant="primary">Go Back</Button>
+        </Link>
+      </div>
+
+
+    );
+  }
+}
