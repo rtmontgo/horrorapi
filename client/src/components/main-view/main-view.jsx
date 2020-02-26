f82c342abc0210e771f77002c25a6b3ff599a5c6
Taylor <r.taylor.montgomery@gmail.com>
2020-02-26 00:37:28 -0600

    update main view

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 
+import { connect } from 'react-redux';
+
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import { setMovies } from '../../actions/actions';
