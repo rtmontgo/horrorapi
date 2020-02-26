f285cf38284cc1e06bdad3b8981a74487a721318
Taylor <r.taylor.montgomery@gmail.com>
2020-02-26 00:30:54 -0600

    update movies list

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -12,7 +12,7 @@ import './index.scss';
 const store = createStore(moviesApp);
 
 // Main component (will eventually use all the others)
-class horrorapi extends React.Component {
+class MyFlixApplication extends React.Component {
   render() {
     return (
       <Provider store={store}>
@@ -26,4 +26,4 @@ class horrorapi extends React.Component {
 const container = document.getElementsByClassName('app-container')[0];
 
 // Tells React to render your app in the root DOM element
-ReactDOM.render(React.createElement(horrorapi), container);
+ReactDOM.render(React.createElement(MyFlixApplication), container);
